f6a4b6b683a09fb12a3bfc7950e06244e66982d5
Ana <acorrama@gmail.com>
2023-10-11 18:19:04 +0200

    preparing background

--- Figures/Figure2.R
+++ Figures/Figure2.R
@@ -1,1 +1,14 @@
-# hello world
+options(stringsAsFactors = FALSE)
+library(Seurat)
+library(patchwork)
+library(ggplot2)
+library(dplyr)
+
+source('Figures/functions_plots.R')
+
+## Data ------------------------------------------------------------------------
+message('Loading data')
+
+all <- readRDS('/home/acorraliza/TOFA_data/20220222_TOFAS_23/00_annotation_process/00_anotadas/todas_2023.RDS')
+
+## Figure 2 A: UMAP subset -----------------------------------------------------
---
+++ Figures/functions_plots.R
@@ -0,0 +1,18 @@
+#
+# colors -----------------------------------------------------------------------
+#
+message('Loading colors')
+
+colors_subset <- c('epi' = '#332f6f',
+                   'stroma' =  '#e28c40',
+                   'tcells' = '#eec35f',
+                   'plasmas' = '#985f9d',
+                   'myeloids' = '#75bccb',
+                   'cycling' = '#7d9f56')
+colors_response <- c('R' = '#70ADE6',
+                     'NR' = '#FF8E47')
+
+#
+# functions --------------------------------------------------------------------
+#
+message('Loading functions')
