bd2c23987a8eb180a92f719e1969e86cbe3c509a
Ana <acorrama@gmail.com>
2023-10-12 15:54:05 +0200

    Figure 3A with colors

--- Figures/Figure_3.R
+++ Figures/Figure_3.R
@@ -21,3 +21,47 @@ myeloids$subset <- factor(myeloids$subset, levels = c('epi', 'stroma', 'plasmas'
 ## Figure 3A  ------------------------------------------------------------------
 # UMAP split by response and treatment, with some celltypes in colour
 # HS, Inflammatory monocytes, M0, M1, M2, IDA macrophages, Neutrophils
+
+colors <- c('HS' = '#bed0e8',
+            'Inflammatory monocytes' = '#eb6c92',
+            'M0' = '#c1d665',
+            'M1' = '#053370',
+            'M2' = '#018d7e',
+            'IDA macrophages' = '#f3bf5d',
+            'Neutrophils' = '#ba4042')
+
+resp_pre <- DimPlot(myeloids[,myeloids@meta.data$response == 'R' & myeloids@meta.data$pre_post == 'PRE'],
+                    group.by = 'annotation_intermediate', pt.size = 0.5) +
+  scale_color_manual(values = colors, na.value = '#e6e5e5') +
+  theme_umap()
+
+nresp_pre <- DimPlot(myeloids[,myeloids@meta.data$response == 'NR' & myeloids@meta.data$pre_post == 'PRE'],
+                    group.by = 'annotation_intermediate', pt.size = 0.5) +
+  scale_color_manual(values = colors, na.value = '#e6e5e5') +
+  theme_umap()
+
+resp_post <- DimPlot(myeloids[,myeloids@meta.data$response == 'R' & myeloids@meta.data$pre_post == 'POST'],
+                    group.by = 'annotation_intermediate', pt.size = 0.5) +
+  scale_color_manual(values = colors, na.value = '#e6e5e5') +
+  theme_umap()
+
+nresp_post <- DimPlot(myeloids[,myeloids@meta.data$response == 'NR' & myeloids@meta.data$pre_post == 'POST'],
+                    group.by = 'annotation_intermediate', pt.size = 0.5) +
+  scale_color_manual(values = colors, na.value = '#e6e5e5') +
+  theme_umap()+
+  theme(legend.position = 'left')
+
+fig3a <- patchwork::wrap_plots(resp_pre, nresp_pre, resp_post, nresp_post, guides = 'collect') &
+  theme(text = element_text(family = 'Helvetica', size = 8))
+
+fig3a_nl <- fig3a & theme(legend.position = 'none')
+
+save_sizes(plot = fig3a, filename = 'Figure_3A', device = 'jpeg')
+save_sizes(plot = fig3a, filename = 'Figure_3A', device = 'tiff')
+save_sizes(plot = fig3a, filename = 'Figure_3A', device = 'svg')
+save_sizes(plot = fig3a, filename = 'Figure_3A', device = 'pdf')
+
+save_sizes(plot = fig3a_nl, filename = 'Figure_3A_no_legend', device = 'jpeg')
+save_sizes(plot = fig3a_nl, filename = 'Figure_3A_no_legend', device = 'tiff')
+save_sizes(plot = fig3a_nl, filename = 'Figure_3A_no_legend', device = 'svg')
+save_sizes(plot = fig3a_nl, filename = 'Figure_3A_no_legend', device = 'pdf')
